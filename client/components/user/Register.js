1064a548c5ddfa0b59211513cd1687df8467d18e
akeelm <akeelm_uk@hotmail.com>
2017-01-06 14:22:44 +0000

    Sorted out confirm password validation

--- client/components/user/Register.js
+++ client/components/user/Register.js
@@ -4,14 +4,29 @@ import InputText from './../forms/InputText.js';
 import { Field, reduxForm } from 'redux-form';
 import * as validate from './../../constants/validate.js';
 
+
 class Register extends React.Component{
+  passwordsMatch(elements) {
+    console.log("fired");
+    if (elements !== undefined &&
+    elements.password !== elements.confirmPassword) {
+      return "<span className=\"text-danger\">Passwords do not match</span>";
+    }
+  }
   handleSubmit(values) {
+    if (values.password !== values.confirmPassword) {
+      console.log(values);
+      this.setState({errors : "Passwords do not match"});
+      return;
+    }
     this.props.loginUser(values.email, values.password);
   }
   render() {
-    const { handleSubmit, submitting } = this.props;
+    const {
+      fields: { password, confirmPassword },
+      handleSubmit, submitting, errors } = this.props;
     return (
-      <form ref="loginForm" className="form-horizontal"
+      <form ref="registerForm" className="form-horizontal"
         onSubmit={this.props.handleSubmit(this.handleSubmit.bind(this))}>
         <div className="section">
           <div className="container">
@@ -27,9 +42,9 @@ class Register extends React.Component{
 
                     <InputText placeholder="E-mail" name="email" validate={[validate.email, validate.required]}/>
 
-                    <InputText placeholder="Password" name="password" validate={validate.required} type="password"/>
+                    <InputText placeholder="Password" name="password" validate={validate.required} type="password" />
 
-                    <InputText placeholder="Confirm password" name="password" validate={validate.required} type="password"/>
+                    <InputText placeholder="Confirm password" name="confirmPassword" validate={validate.required, validate.validateConfirm} type="password" />
 
                     <div className="form-group">
                       <div className="col-md-10 col-md-offset-2">
@@ -51,9 +66,11 @@ class Register extends React.Component{
 };
 
 Register = reduxForm({
-  form: 'registerForm', // a unique name for this form
-  touchOnBlue: false,
-  touchOnChange: false
+  form: 'registerForm',
+  fields: [
+    'password', 'confirmPassword'
+  ],
+  validate: validate.validateConfirm
 })(Register);
 
 
